8eeb565eb63fff6c00e20f5321b53c9fac716c62
Retchiele Datuin <rldatuin@gmail.com>
2021-12-07 23:36:52 -0800

    backend - login

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/user.model')
+const bcrypt = require('bcrypt')
 
 module.exports = {
     register(req, res) {
@@ -11,5 +12,29 @@ module.exports = {
                 console.log("new user: ", user._id)
             })
             .catch(err => res.json(err));
+    },
+
+    async login(req, res) {
+        const {email, password} = req.body;
+        const errorMessage = 'Please check your email or password';
+        try {
+            // checks db for user by email
+            const user = await User.findOne({email})
+            console.log(user);
+            if(user === null) {
+                throw new Error(errorMessage)
+            }
+            // checks db and compares input with user password
+            const result = await bcrypt.compare(password, user.password)
+            if(result === false) {
+                throw new Error(errorMessage)
+            }
+            res.json({status: 'Success', userId: user._id})
+        } catch(e) {
+            res
+                .status(400)
+                .json({message: errorMessage})
+        }
+
     }
 }
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,4 +2,5 @@ const UC = require('../controllers/user.controller')
 
 module.exports = app => {
     app.post('/api/register', UC.register);
+    app.post('/api/login', UC.login);
 }
